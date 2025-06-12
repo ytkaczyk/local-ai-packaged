6709ca760dd1cefc6b159f885c4727c3af19d709
Yves Tkaczyk <yves@upswing.io>
2025-06-12 12:31:01 -0600

    Add arguments to volume mapper

--- volume-mapper.py
+++ volume-mapper.py
@@ -1,10 +1,11 @@
-from ruamel.yaml import YAML, CommentedMap as OrderedDict, CommentedSeq as OrderedList
+from ruamel.yaml import YAML, CommentedMap as OrderedDict
+import argparse
+import os
 
-def main():
+def add_root_to_volumes(input: str, output:str, root_path: str):
     yaml=YAML(typ='rt')   # default, if not specfied, is 'rt' (round-trip)
-    with open("./docker-compose.yml", "r") as file:
+    with open(input, "r") as file:
         data = yaml.load(file)
-    print(data)
 
     for volume in data.get('volumes'):
         if data["volumes"][volume] is None:
@@ -13,12 +14,18 @@ def main():
                 "driver_opts": OrderedDict({
                     "type": "none",
                     "o": "bind",
-                    "device": f"/var/localai/{volume}"
+                    "device": os.path.join(root_path, volume)
                 })
             })
 
-    with open("./docker-compose2.yml", "w") as file:
+    with open(output, "w") as file:
         yaml.dump(data, file)
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Description of your program")
+    parser.add_argument("--input", type=str, default="./docker-compose.yml", help="Input YAML file")
+    parser.add_argument("--output", type=str, default="./docker-compose2.yml", help="Output YAML file")
+    parser.add_argument("--root", type=str, default="/var/localai", help="Root path for volume mapping")
+    args = parser.parse_args()
+
+    add_root_to_volumes(args.input, args.output, args.root)
